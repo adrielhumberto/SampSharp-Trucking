00f2642e49764c830076eee6dd92177aea518c7c
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-12-03 13:51:24 +0200

    Initial commit

---
+++ src/TruckingGameMode/GameMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Controllers;
+
+namespace TruckingGameMode
+{
+    public class GameMode : BaseMode
+    {
+        protected override void LoadControllers(ControllerCollection controllers)
+        {
+            base.LoadControllers(controllers);
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+    }
+}
---
+++ src/TruckingGameMode/Program.cs
@@ -0,0 +1,14 @@
+﻿using SampSharp.Core;
+
+namespace TruckingGameMode
+{
+    class Program
+    {
+        static void Main()
+        {
+            new GameModeBuilder()
+                .Use<GameMode>()
+                .Run();
+        }
+    }
+}
