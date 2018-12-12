e2526a24295da3185ff8192b30108a9a28cae5fc
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-12-12 19:00:09 +0200

    Added initial database layout

---
+++ src/GamemodeDatabase/GamemodeContext.cs
@@ -0,0 +1,17 @@
+﻿using GamemodeDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamemodeDatabase
+{
+    public class GamemodeContext : DbContext
+    {
+        public DbSet<PlayerModel> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=trucking;Uid=root;Pwd=;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
