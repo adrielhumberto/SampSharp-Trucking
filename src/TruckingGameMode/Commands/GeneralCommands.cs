79f212be5e026f5ab2254fb3427669a381736358
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-12-16 17:38:50 +0200

    Added pm command

---
+++ src/TruckingGameMode/Commands/GeneralCommands.cs
@@ -0,0 +1,21 @@
+﻿using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.World;
+
+namespace TruckingGameMode.Commands
+{
+    public class GeneralCommands
+    {
+        [Command("pm")]
+        public static void OnPmCommand(BasePlayer sender, BasePlayer receiver, string message)
+        {
+            if (sender.Id == receiver.Id)
+            {
+                sender.SendClientMessage(Color.IndianRed, "You can't pm yourself!");
+                return;
+            }
+
+            receiver.SendClientMessage(Color.White, $"{Color.Gray}PM from {sender.Name}({sender.Id}): {Color.White}{message}");
+        }
+    }
+}
