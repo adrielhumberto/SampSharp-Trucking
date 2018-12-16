337ee2cef4e21bd716d10791b6db34a659bab843
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2018-12-16 17:49:54 +0200

    Added flip command

--- src/TruckingGameMode/Commands/GeneralCommands.cs
+++ src/TruckingGameMode/Commands/GeneralCommands.cs
@@ -1,4 +1,5 @@
-﻿using SampSharp.GameMode.SAMP;
+﻿using SampSharp.GameMode.Definitions;
+using SampSharp.GameMode.SAMP;
 using SampSharp.GameMode.SAMP.Commands;
 using SampSharp.GameMode.World;
 
@@ -16,6 +17,22 @@ namespace TruckingGameMode.Commands
             }
 
             receiver.SendClientMessage(Color.White, $"{Color.Gray}PM from {sender.Name}({sender.Id}): {Color.White}{message}");
+        }
+
+        [Command("flip")]
+        public static void OnFlipCommand(BasePlayer sender)
+        {
+            if (sender.State != PlayerState.Driving)
+            {
+                sender.SendClientMessage(Color.IndianRed, "You need to drive a car!");
+                return;
+            }
+
+            if (sender.InAnyVehicle)
+                sender.PutCameraBehindPlayer();
+
+            sender.Vehicle.Position = sender.Position;
+            sender.Vehicle.Angle = 0.0f;
         }
     }
 }
