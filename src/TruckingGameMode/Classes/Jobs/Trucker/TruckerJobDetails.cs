ca60c56ddd684de19fc53a213452bd645dc616b4
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2019-01-12 15:50:01 +0200

    Removed unnecessary variables

--- src/TruckingGameMode/Classes/Jobs/Trucker/TruckerJobDetails.cs
+++ src/TruckingGameMode/Classes/Jobs/Trucker/TruckerJobDetails.cs
@@ -32,22 +32,19 @@ namespace TruckingGameMode.Classes.Jobs.Trucker
         public static List<TruckerJobDetails> GenerateJobList(TruckerJobLocation startPoint)
         {
             var list = new List<TruckerJobDetails>(10);
-            var randomMoney = new Random();
-            var randomWeight = new Random();
-            var randomDestination = new Random();
-            var randomCargo = new Random();
+            var random = new Random();
 
             for (var i = 0; i < 10; i++)
             {
-                var destination = TruckerJobLocation.JobLocations[randomDestination.Next(TruckerJobLocation.JobLocations.Count)];
+                var destination = TruckerJobLocation.JobLocations[random.Next(TruckerJobLocation.JobLocations.Count)];
                 if(destination.Position.DistanceTo(startPoint.Position) <= 1.0)
-                    destination = TruckerJobLocation.JobLocations[randomDestination.Next(TruckerJobLocation.JobLocations.Count)];
+                    destination = TruckerJobLocation.JobLocations[random.Next(TruckerJobLocation.JobLocations.Count)];
 
                 list.Add(new TruckerJobDetails(startPoint, 
                         destination, 
-                        TruckerCargo.Cargoes[randomCargo.Next(TruckerCargo.Cargoes.Count)], 
-                        randomMoney.Next(30, 140), 
-                        randomWeight.Next(1, 22)));
+                        TruckerCargo.Cargoes[random.Next(TruckerCargo.Cargoes.Count)], 
+                        random.Next(30, 140), 
+                        random.Next(1, 22)));
             }
 
             return list;
