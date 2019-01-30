10b1b71a61647de51065eeeea0d910706fa43366
Neagoe Ciprian <neagoeciprian16@yahoo.com>
2019-01-30 18:18:49 +0200

    Added setlevel command  to level 3 admin

---
+++ src/TruckingGameMode/Commands/AdminCommands/AdminCommandPermissions/LevelThreeAdminPermission.cs
@@ -0,0 +1,16 @@
+﻿using SampSharp.GameMode.SAMP.Commands.PermissionCheckers;
+using SampSharp.GameMode.World;
+using TruckingGameMode.World;
+
+namespace TruckingGameMode.Commands.AdminCommands.AdminCommandPermissions
+{
+    public class LevelThreeAdminPermission : IPermissionChecker
+    {
+        public bool Check(BasePlayer player)
+        {
+            return player is Player playerData && playerData.PlayerData().AdminLevel == 3;
+        }
+
+        public string Message => "You need level 3 admin level to use this command";
+    }
+}
---
+++ src/TruckingGameMode/Commands/AdminCommands/LevelThreeAdminCommands.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using GamemodeDatabase;
+using MySql.Data.MySqlClient;
+using SampSharp.GameMode.SAMP;
+using SampSharp.GameMode.SAMP.Commands;
+using SampSharp.GameMode.World;
+using TruckingGameMode.World;
+
+namespace TruckingGameMode.Commands.AdminCommands
+{
+    [CommandGroup("admin", PermissionChecker = typeof(LevelThreeAdminCommands))]
+    public class LevelThreeAdminCommands
+    {
+        [Command("setadmin", Shortcut = "setadmin")]
+        public static void OnSetAdminCommand(BasePlayer sender, Player playerId, int level)
+        {
+            if (level < 0 || level > 3)
+            {
+                sender.SendClientMessage(Color.IndianRed, "Invalid admin level.");
+                return;
+            }
+
+            if (sender.Id == playerId.Id)
+            {
+                sender.SendClientMessage(Color.IndianRed, "You can't set yourself admin.");
+                return;
+            }
+
+            if (playerId.PlayerData().AdminLevel == 3)
+            {
+                sender.SendClientMessage(Color.IndianRed, "You can't set admin level to highest admin.");
+                return;
+            }
+
+            using (var db = new MySqlConnection(DapperHelper.ConnectionString))
+            {
+                db.Execute(@"UPDATE players SET AdminLevel = @Level WHERE Name = @PName", new{Level = level, PName = playerId.Name});
+            }
+
+            playerId.SendClientMessage(Color.GreenYellow,
+                level > playerId.PlayerData().AdminLevel
+                    ? $"You got promoted to admin level {level} by {sender.Name}."
+                    : $"You got demoted to admin level {level} by {sender.Name}.");
+
+            sender.SendClientMessage(Color.GreenYellow, $"You set admin level {level} to {playerId.Name}.");
+        }
+    }
+}
